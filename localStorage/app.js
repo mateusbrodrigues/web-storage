e5b172302d7f639bf489de7d8639b3dbd2de76cf
Mateus Rodrigues <mateusborges090@gmail.com>
2024-07-13 16:32:20 -0300

    map values to html table

--- localStorage/app.js
+++ localStorage/app.js
@@ -1,6 +1,7 @@
 class App {
   constructor() {
     this.initializeForm();
+    this.listValues();
   }
 
   initializeForm() {
@@ -9,6 +10,7 @@ class App {
       event.preventDefault();
       console.log("form submitted");
       this.save({ key: form.key.value, value: form.keyValue.value });
+      this.listValues();
       form.reset();
       form.key.focus();
     });
@@ -16,8 +18,29 @@ class App {
 
   save({ key, value }) {
     console.log("saving data...");
-    console.log({ key, value });
     window.localStorage.setItem(key, value);
+  }
+
+  listValues() {
+    console.log("listing data...");
+    const ls = window.localStorage;
+    if (!ls.length) return;
+    const lsKeys = Object.keys(ls);
+    const allValues = lsKeys.map(this.toHTML).join("");
+    // this.addToHTML(allValues);
+  }
+
+  toHTML(key) {
+    const value = window.localStorage.getItem(key);
+    const html = `<p>${key}: ${value}</p>`;
+    return html;
+  }
+
+  addToHTML(allValues) {
+    console.log("adding to HTML...");
+    const listValues = document.getElementById("listValues");
+    listValues.innerHTML;
+    listValues.insertAdjacentHTML("beforeend", allValues);
   }
 }
 
